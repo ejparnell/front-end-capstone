adf34b0c88601f788a5de76cfe339aadd798e317
ejparnell <ejparnell@hotmail.com>
2019-06-05 13:40:38 -0400

    Changed the apiConfig to point to my heroku

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://afternoon-stream-50589.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
