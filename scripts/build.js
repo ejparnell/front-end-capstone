0da6f9e94afa4992e668ac29c6ec83f4442c7f3e
ejparnell <ejparnell@hotmail.com>
2019-06-06 11:41:47 -0400

    Fixed build

--- scripts/build.js
+++ scripts/build.js
@@ -61,12 +61,12 @@ measureFileSizesBeforeBuild(paths.appBuild)
           '\nSearch for the ' +
             chalk.underline(chalk.yellow('keywords')) +
             ' to learn more about each warning.'
-        )
+
         // console.log(
           'To ignore, add ' +
             chalk.cyan('// eslint-disable-next-line') +
             ' to the line before.\n'
-        )
+        
       } else {
         // console.log(chalk.green('Compiled successfully.\n'))
       }
